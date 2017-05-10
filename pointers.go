6bf849f1a164d4987cf1b7bb13e6faf022b3036c
John Smith <fygol@users.noreply.github.com>
2017-05-10 23:14:35 +0300

    Create pointers.go

---
+++ pointers.go
@@ -0,0 +1,24 @@
+// pointer is a variable which stores the address of another variable.
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	a := 10
+
+	fmt.Println(a) // 10
+
+	var p *int
+	fmt.Println(p) // <nil>
+	
+	// & - address operator
+	p = &a // returns *int (pointer to int); the same as var p *int = &a
+	fmt.Println(p) // 0xc42000a2f0
+	fmt.Println(reflect.TypeOf(p)) // *int
+
+	*p = 11 // dereference pointer variable, the same as a = 11
+	fmt.Println(a) // 11
+}
